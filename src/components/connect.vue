043626be6a1e1fb21f1d1b09b6f2a347bfe57b16
阿坤 <2207772814@qq.com>
2020-07-31 15:37:19 +0800

    Initial commit

---
+++ src/components/connect.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="content">
+    </div>
+</template>
+
+<script>
+export default {
+  name: ''
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/nav.vue
@@ -0,0 +1,108 @@
+<template>
+  <div >
+   <!-- <div class="beijinhg">
+    </div>-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <img src="../assets/loge.jpg" height="30px" width="30px">
+      <a class="navbar-brand" href="#">Dustandsnow</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item" v-for="(nav,index) in navs" v-on:click="gaolaing(index)" :key="nav.title">
+            <a class="nav-link" href="#">{{nav.title}}</a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown" v-if="nav.xiala">
+              <a class="dropdown-item" href="#" v-for="chile in nav.child" :key="chile">{{chile}}</a>
+            </div>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="请输入关键字" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">查询</button>
+        </form>
+      </div>
+    </nav>
+    <div class="content">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data: function () {
+    return {
+      navs: [{
+        title: '主页',
+        href: '#'
+      },
+      {
+        title: '文章',
+        href: '#'
+      },
+      {
+        title: '笔记',
+        href: '#'
+      },
+      {
+        title: '留言',
+        href: '#',
+        xiala: 'ture',
+        child: {
+          title1: 'Action',
+          title2: 'Another',
+          title3: 'Something else'
+        }
+      }]
+    }
+  },
+  created () {
+    if (document.readyState === 'complete') {
+      this.xiala()
+    } else {
+      document.addEventListener('load', () => this.xiala())
+    }
+  },
+  mounted () {
+    this.xiala()
+  },
+  methods: {
+    gaolaing: function (n) {
+      this.$('li').removeClass('active')
+      this.$('li')[n].addClass('active')
+    },
+    xiala: function () {
+      this.$('.dropdown-menu').parent().addClass('dropdown')
+      this.$('.dropdown-menu').prev().addClass('dropdown-toggle').attr({
+        'id': 'navbarDropdown',
+        'role': 'button',
+        'data-toggle': 'dropdown',
+        'aria-haspopup': 'true',
+        'aria-expanded': 'false'
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  .beijinhg{
+    width: 90%;
+    height: 300px;
+    background-image: url("../assets/zhuye.jpg") ;
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+    margin: auto;
+  }
+  nav{
+    margin: 10px 20px 10px 20px;
+  }
+  image{
+    margin: 8px;
+  }
+</style>
